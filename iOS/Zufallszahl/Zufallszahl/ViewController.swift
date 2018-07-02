cd70fc2b7381396db09d9efd4357550b58a827ef
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-07-02 13:06:20 +0200

    iOS version added (not finished yet)

---
+++ iOS/Zufallszahl/Zufallszahl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zufallszahl
+//
+//  Created by Thomas Künneth on 29.06.18.
+//  Copyright © 2018 Thomas Künneth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
