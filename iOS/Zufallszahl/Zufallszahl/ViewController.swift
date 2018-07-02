0756506cc3645394df62bd0e65e603ccea17a264
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-07-02 13:48:48 +0200

    iOS version finished - now calls the function, too

--- iOS/Zufallszahl/Zufallszahl/ViewController.swift
+++ iOS/Zufallszahl/Zufallszahl/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import FirebaseFunctions
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
+    lazy var functions = Functions.functions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        functions.httpsCallable("zufallszahl").call(["max": 10 as Int16]) { (result, error) in
+            if let error = error as NSError? {
+                if error.domain == FunctionsErrorDomain {
+                    // ...
+                }
+            }
+            if let result = (result?.data as? [String: Any])?["result"] as? Int16 {
+                self.label.text = String(result)
+            }
+        }
+    }
 }
 
